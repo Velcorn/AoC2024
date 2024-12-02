d9631bccbbc809651f506a9bb6c978b17e739677
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-02 03:18:39 +0100

    Initial commit

---
+++ Day01/day01.py
@@ -0,0 +1,2 @@
+# Split input into two lists of integers, sort lists and print the sum of pairwise distances
+print(sum(abs(x - y) for x, y in zip(*map(sorted, zip(*[map(int, line.split()) for line in open('input.txt')])))))
