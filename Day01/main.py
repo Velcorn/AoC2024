da483e2dc5e24aac64d880102b44fd2e7011c06b
velcorn <8927053+Velcorn@users.noreply.github.com>
2024-12-03 01:55:05 +0100

    Day 1 Part Two

--- Day01/main.py
+++ Day01/main.py
@@ -1,2 +1,11 @@
-# Split input into two lists of integers, sort lists and calculate the sum of pairwise distances
-print(sum(abs(x - y) for x, y in zip(*map(sorted, zip(*[map(int, line.split()) for line in open('input.txt')])))))
+from collections import Counter
+
+# Read input file and split into two lists
+with open('input.txt') as f:
+    llist, rlist = zip(*((int(num1), int(num2)) for num1, num2 in (line.split() for line in f)))
+
+# Part One: Calculate sum of pairwise distances after sorting
+print(sum(abs(x - y) for x, y in zip(sorted(llist), sorted(rlist))))
+
+# Part Two: Calculate the sum of left list numbers multiplied by occurrences in the right list
+print(sum(num * Counter(rlist)[num] for num in llist))
