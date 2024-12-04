1afd3a43e86c2c887e42c5b209befcd018424c32
Velcorn <Velcorn@users.noreply.github.com>
2024-12-04 20:44:15 +0100

    Day 4 Part One and Two

--- Day03/day03.py
+++ Day03/day03.py
@@ -10,7 +10,6 @@ pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don\'t\(\))")
 # Part One: Calculate the sum of pairwise multiplications of numbers
 print(f"Part One: {sum(int(item[0]) * int(item[1]) for item in pattern.findall(text) if item[0])}")
 
-
 # Part Two: Calculate the sum of pairwise multiplications of numbers depending on the do and don't functions
 items = pattern.findall(text)
 do = True
---
+++ Day04/day04.py
@@ -0,0 +1,24 @@
+# Read input file
+with open('input.txt') as f:
+    text = f.read()
+
+# Part One: Extract all horizontal, vertical and diagonal (from left to right and right to left) lines from the input,
+# then count the occurrences of the word "XMAS" or "SAMX" (backwards) in all lines
+lines = text.splitlines()
+horizontal = lines
+vertical = [''.join([line[i] for line in lines]) for i in range(len(lines[0]))]
+diagonal_lr = [''.join([line[i + j] for j, line in enumerate(lines) if 0 <= i + j < len(line)])
+               for i in range(-len(lines), len(lines[0]))]
+diagonal_rl = [''.join([line[i - j] for j, line in enumerate(lines) if 0 <= i - j < len(line)])
+               for i in range(len(lines[0]) + len(lines) - 1)]
+xmas_count = sum(line.count('XMAS') + line.count('SAMX') for line in horizontal + vertical + diagonal_lr + diagonal_rl)
+print(f"Part One: {xmas_count}")
+
+# Part Two: Extract all 3x3 Xs and count the number of Xs that consist of only "MAS" or "SAM"
+xs = [lines[i][j] + lines[i + 1][j + 1] + lines[i + 2][j + 2] + lines[i + 2][j] + lines[i + 1][j + 1] + lines[i][j + 2]
+      for i in range(len(lines) - 2) for j in range(len(lines[0]) - 2)]
+x_mas_count = 0
+for x in xs:
+    if x.count('MAS') + x.count('SAM') == 2:
+        x_mas_count += 1
+print(f"Part Two: {x_mas_count}")
